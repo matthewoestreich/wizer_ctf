102bc25444976e22d1e4d5fb6ff33a5774c0cc9c
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-11 15:55:23 -0500

    Untrack files in .gitignore

--- challenge_48/test.js
+++
@@ -1,3 +0,0 @@
-const baseUrl = "https://gist.githubusercontent.com/matthewoestreich/9917f6563b2b73a3b59b7ceff7979a6b/raw/8b854b9b22987b6d1eca1a3c0e2026ef6f89659e"
-
-const payload = `<base href="https://rtchat-pentest.onrender.com/" />`;
